d413bc187caf00b4e16e8ecd73934c901346829f
Mojtaba Ahmadzadeh <mojtabaahmadzadeh.2024@gmail.com>
2025-05-07 14:51:12 +0330

    Add new feature for Chat AI

---
+++ src/app/login/page.jsx
@@ -0,0 +1,12 @@
+import Login from '@/components/templates/Accounts/Login'
+import React from 'react'
+
+function LoginAuth() {
+    return (
+        <>
+            <Login />
+        </>
+    )
+}
+
+export default LoginAuth
---
+++ src/app/register/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Register from '@/components/templates/Accounts/Register'
+
+function RegisterAuth() {
+  return (
+    <>
+    <Register/>
+    </>
+  )
+}
+
+export default RegisterAuth
